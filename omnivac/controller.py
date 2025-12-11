d411c7f6cc21ccbba4226bbfa60f99b00742863c
Nico Stoedter <nicht vorhanden>
2025-12-11 14:46:59 +0100

    Controller.py verbessert. Abhänggige funktionen müssen angepasst werden

--- omnivac/controller.py
+++ omnivac/controller.py
@@ -11,22 +11,221 @@ pygame.joystick.init()
 
 class ControllerManager:
 
-    def __init__(self, motor_manager: "MotorManager", ini_manager, label_list, pop_up, **kwargs):
-        self.double_speed = False      
+    def __init__(self, motor_manager: "MotorManager", ini_manager, label_list, pop_up, **kwargs):      
         self.setting = False            # If false controller has axis 1-5 and 6-10 otherwise
+        self.double_speed = False
 
         self.pop_up = pop_up
         self.ini_manager = ini_manager
         self.motor_manager = motor_manager
         self.label_list = label_list
         self.joystick = None
-
         self.motors: dict[int, "Motor"] = self.motor_manager.motors
+        self.moving_motor: dict[int, bool] = {key: False for key in self.motors}
+        self.last_spd_send: dict[int, int] = {key: 0 for key in self.motors}
+
+    def controller(self) -> None:
+        '''Manages Joystick Inputs and Calls Joystick Funktions by itself'''
+        for event in pygame.event.get(): # All Joystick inputs are in pygame.event
+
+            axis_x = 0
+            axis_y = 0
+
+            # No Joystick inputs if pop_up is visible
+            if self.pop_up and self.pop_up.is_visible():
+                continue
+
+            # --- Add/Disconnect Joystick
+            if event.type == pygame.JOYDEVICEREMOVED:
+                print('DISCONECTED')
+
+            elif event.type == pygame.JOYDEVICEADDED:
+                self.event_controller_connected(event)
+
+            # --- Button Managment ---
+            elif event.type == pygame.JOYBUTTONDOWN:    # Events if button pressed
+                if event.button == 4:
+                    self.event_button_lb()
+
+                elif event.button == 5:
+                    self.event_button_rb_down()
+
+            elif event.type == pygame.JOYBUTTONUP:  # Events if button released
+                if event.button == 5:
+                    self.event_button_rb_up()
+
+            # --- Joystick Managment ---
+            elif event.type == pygame.JOYAXISMOTION:    # Joystick Motion
+                parsed_event_code = self.gamepad_parser(event.axis)
+                motor_id = self.find_motor_with_axis(parsed_event_code)
+
+                if motor_id == None:
+                    break
+
+                spd_tuple, id_tuple = self.joystick_motion(event.axis, motor_id)                      
+
+                if motor_id in[74,75]: # Hier späte gucken ob einfacher geht
+                    axis_x = round(self.joystick.get_axis(0), 2)
+                    axis_y = round(self.joystick.get_axis(1) * -1, 2)
+                        
+                #self.motor_manager.controller_movement(axis_x, axis_y, spd_tuple, id_tuple motor_id)
+
+    # --- Button Events ---
+
+    def event_controller_connected(self, event):
+        '''Manages adding joystick'''
+        motor_index = event.device_index
+        self.joystick = pygame.joystick.Joystick(motor_index)
+        self.joystick.init()
+
+    def event_button_lb(self):
+        '''Manages action for lb button'''
+        print(f'[ControllerManager] switched axis layout')
+        self.setting = not(self.setting)
+        self.switch_label_color()
+    
+    def event_button_rb_down(self):
+        '''Manages action for lb button'''
+        print(f'[ControllerManager] double speed ON')
+        self.double_speed = True
+
+        for key, value in self.last_spd_send.items():
+            if self.moving_motor.get(key):
+                print("Cool")
+                axis_x = 0
+                axis_y = 0
+                gradual_spd_x = 0
+                gradual_spd_y = 0
+                new_spd = value * 2
+                self.motor_manager.controller_movement(axis_x, axis_y, gradual_spd_x, 
+                                                       gradual_spd_y, key, new_spd)
+                self.last_spd_send[key] = new_spd
+
+    def event_button_rb_up(self):
+        '''Manages action for lb button'''
+        print(f'[ControllerManager] double speed OFF')
+        self.double_speed = False
+
+        for key, value in self.last_spd_send.items():
+            if self.moving_motor.get(key):
+                axis_x = 0
+                axis_y = 0
+                gradual_spd_x = 0
+                gradual_spd_y = 0
+                new_spd = int(value/2)
+                self.motor_manager.controller_movement(axis_x, axis_y, gradual_spd_x, 
+                                                       gradual_spd_y, key, new_spd)
+                self.last_spd_send[key] = new_spd
+
+    def joystick_motion(self, joy_axis: int, motor_id: int) -> tuple:
+        '''Manages value from joystick'''
+        spd_rpm: float = float(self.ini_manager.get_value(motor_id, "Soft_Basic", "Max_Speed(rpm)"))
+        spd_stepps = max(-32768, min(int(spd_rpm / 60 * 2000), 32767))
+
+        spd_x = spd_y = spd_z = 0
+
+        x_id = y_id = z_id = None
+
+        # --- Axis Management ---
+        axes = { # Get value from all axis
+            0: round(self.joystick.get_axis(0), 2),
+            1: round(self.joystick.get_axis(1) * -1, 2),
+            2: round(self.joystick.get_axis(2), 2),
+            3: round(self.joystick.get_axis(3) * -1, 2),
+            4: round(self.joystick.get_axis(4), 2),
+            5: round(self.joystick.get_axis(5), 2),
+        }
+
+        # Stick-Layout: (x_axis, y_axis)
+        stick_map = {
+            0: (0, 1),  # joy_axis 0 or 1 → Stick 1
+            1: (0, 1),
+            2: (2, 3),  # joy_axis 2 or 3 → Stick 2
+            3: (2, 3),
+        }
+
+        factor = 1 if not self.double_speed else 2
+
+        # --- Stick (X-Axis, Y-Axis) ---
+        if joy_axis in stick_map:
+            x_axis, y_axis = stick_map[joy_axis]
+
+            x_value = axes[x_axis]
+            y_value = axes[y_axis]
+
+            parsed_x_axis = self.gamepad_parser(x_axis)
+            parsed_y_axis = self.gamepad_parser(y_axis)
+            x_id = self.find_motor_with_axis(parsed_x_axis)
+            y_id = self.find_motor_with_axis(parsed_y_axis)
+
+            self.moving_motor[x_id] = x_value > 0
+            self.moving_motor[y_id] = y_value > 0
+
+            spd_x = factor * int((spd_stepps / 2) * x_value)
+            spd_y = factor * int((spd_stepps / 2) * y_value) 
+
+        # --- Trigger (Z-Axis) ---
+        elif joy_axis in [4, 5]:
+            z_axis = axes[4] if axes[4] > 0 else axes[5]
+            parsed_z_axis = self.gamepad_parser(joy_axis)
+            z_id = self.find_motor_with_axis(parsed_z_axis)
+            self.moving_motor[motor_id] = z_axis > 0
+            spd_z = factor * self.event_joy_correction_rt_lt(z_axis, spd_stepps)
+
+            # Left Trigger = negative value
+            if joy_axis == 4:
+                spd_z *= -1
+
+        print((spd_x, spd_y, spd_z), (x_id, y_id, z_id))
+
+        return (spd_x, spd_y, spd_z), (x_id, y_id, z_id)
+
+    # --- Miscellaneous ---
+
+    def event_joy_correction_rt_lt(self, event_axis, spd_stepps):
+        # Trigger: RT = Axis 5, LT = Axis 4 
+        # Special because weird values. Sticks have -1.0 to 1.0 but for RT/LT have each -1.0 to 1.0
+        trigger_value = (event_axis + 1) / 2 if event_axis != 0 else 0
+
+        if trigger_value > 0.1:
+            gradual_spd = int((spd_stepps / 2) * trigger_value)
+        else:
+            gradual_spd = 0
+
+        return gradual_spd
+    
+    def find_motor_with_axis(self, axis) -> int:
+        for id, motor in self.motors.items():
+            if motor.joystick_axis == int(axis):
+                return id
+            
+    def switch_label_color(self) -> None:
+        '''Switches the labels on the front end''' #Todo funktioniert noch nicht richtig
+        for label in self.label_list:
+            if self.setting:
+                if label in self.label_list[5:]:
+                    text = label.text()
+                    text = ControllerManager.clear_text(text)
+                    label.setText(f"<font color='yellow'>{text}</font>")
+                else:
+                    text = label.text()
+                    text = ControllerManager.clear_text(text)
+                    label.setText(f"<font color='white'>{text}</font>")
+            else:
+                if label in self.label_list[:5]:
+                    text = label.text()
+                    text = ControllerManager.clear_text(text)
+                    label.setText(f"<font color='yellow'>{text}</font>")
+                else:
+                    text = label.text()
+                    text = ControllerManager.clear_text(text)
+                    label.setText(f"<font color='white'>{text}</font>")
 
     def add_joy(self):
-            joystick = pygame.joystick.Joystick(2)
-            joystick.init()
-            print(f"Controller erkannt: {joystick.get_name()}")
+        '''Adds Joystick'''
+        joystick = pygame.joystick.Joystick(2)
+        joystick.init()
+        print(f"Controller erkannt: {joystick.get_name()}")
 
     def gamepad_parser(self, input: int) -> str:
         ''' Parses Gamepad axis to .ini axis'''
@@ -52,148 +251,6 @@ class ControllerManager:
             return parser_2.get(input)
         else:    
             return parser.get(input)
-    
-    def double_speed(self, event_state) -> None:
-        if event_state == 1:
-            self._double_speed = True
-            print('[ControllerManager] Double Speed Enabled')
-        else:
-            self._double_speed = False
-            print('[ControllerManager] Double Speed Disabled')
-
-    def controller(self) -> None:
-        for event in pygame.event.get():
-
-            axis_x = 0
-            axis_y = 0
-            gradual_spd_x = 0
-            gradual_spd_y = 0
-            gradual_spd  = 0
-
-            if self.pop_up and self.pop_up.is_visible():
-                continue
-
-            if event.type == pygame.JOYDEVICEREMOVED:
-                print('DISCONECTED')
-
-            elif event.type == pygame.JOYDEVICEADDED:
-                self.event_controller_connected(event)
-
-            elif event.type == pygame.JOYBUTTONDOWN:    # Events if button pressed
-                if event.button == 4:
-                    self.event_button_lb()
-
-                elif event.button == 5:
-                    self.event_button_rb_down()
-
-            elif event.type == pygame.JOYBUTTONUP:  # Events if button released
-                if event.button == 5:
-                    self.event_button_rb_up
-
-            elif event.type == pygame.JOYAXISMOTION:    # Joystick Motion
-                parsed_event_code = self.gamepad_parser(event.axis)
-                motor_id = self.find_motor_with_axis(parsed_event_code)
-
-                if motor_id == None:
-                    print("Upsi nix motor Id gefunden") # Debug
-                    break
-
-                spd_rpm: float = float(self.ini_manager.get_value(motor_id, "Soft_Basic", "Max_Speed(rpm)"))
-                spd_stepps = max(-32768, min(int(spd_rpm / 60 * 2000), 32767))
-                gradual_spd = int((spd_stepps / 2) * event.value)
-
-                if event.axis in [1,3,4,5]:
-                    gradual_spd = self.event_joy_correction(event, gradual_spd)
-                        
-                if self.double_speed:
-                    gradual_spd = gradual_spd * 2
-
-                if motor_id in[74,75]: # Hier späte gucken ob einfacher geht
-                    axis_x = round(self.joystick.get_axis(0), 2)
-                    axis_y = round(self.joystick.get_axis(1) * -1, 2)
-
-                    x_motor = self.motors.get(74)
-                    y_motor = self.motors.get(75)
-
-                    x_spd_rpm = x_motor.max_speed
-                    y_spd_rpm = y_motor.max_speed
-
-                    spd_stepps_x = max(-32768, min(int(x_spd_rpm / 60 * 2000), 32767))
-                    spd_stepps_y = max(-32768, min(int(y_spd_rpm / 60 * 2000), 32767))
-
-                    gradual_spd_x = int(abs(axis_x) * (spd_stepps_x / 2))
-                    gradual_spd_y = int(abs(axis_y) * (spd_stepps_y / 2))
-                        
-                self.motor_manager.controller_movement(axis_x, axis_y, gradual_spd_x, 
-                                                       gradual_spd_y, motor_id, gradual_spd)
-
-    def find_motor_with_axis(self, axis) -> int:
-        for id, motor in self.motors.items():
-            if motor.joystick_axis == int(axis):
-                return id
-            
-    def switch_label_color(self) -> None:
-        for label in self.label_list:
-            if self.setting:
-                if label in self.label_list[5:]:
-                    text = label.text()
-                    text = ControllerManager.clear_text(text)
-                    label.setText(f"<font color='yellow'>{text}</font>")
-                else:
-                    text = label.text()
-                    text = ControllerManager.clear_text(text)
-                    label.setText(f"<font color='white'>{text}</font>")
-            else:
-                if label in self.label_list[:5]:
-                    text = label.text()
-                    text = ControllerManager.clear_text(text)
-                    label.setText(f"<font color='yellow'>{text}</font>")
-                else:
-                    text = label.text()
-                    text = ControllerManager.clear_text(text)
-                    label.setText(f"<font color='white'>{text}</font>")
-
-    def event_controller_connected(self, event):
-        print('ADDED')
-        motor_index = event.device_index
-        self.joystick = pygame.joystick.Joystick(motor_index)
-        self.joystick.init()
-
-    def event_button_lb(self):
-        print(f'[ControllerManager] switched axis layout')
-        self.setting = not(self.setting)
-        self.switch_label_color()
-    
-    def event_button_rb_down(self):
-        print(f'[ControllerManager] double speed ON')
-        self.double_speed = True
-
-    def event_button_rb_up(self):
-        print(f'[ControllerManager] double speed OFF')
-        self.double_speed = False
-
-    def event_joy_correction(self, event, spd_stepps):
-        # Trigger: RT = Axis 5, LT = Axis 4 (special because weird values)
-        if event.axis == 5:  # RT
-            trigger_value = (event.value + 1) / 2
-
-            if trigger_value > 0.1:
-                gradual_spd = int((spd_stepps / 2) * trigger_value)
-            else:
-                gradual_spd = 0
-
-        elif event.axis == 4:  # LT
-            trigger_value = (event.value + 1) / 2
-
-            if trigger_value > 0.1:
-                gradual_spd = int(-(spd_stepps / 2) * trigger_value)
-            else:
-                gradual_spd = 0
-        elif event.axis == 1 or event.axis == 3:
-            # invert axis 1 and 3
-            gradual_spd = spd_stepps * -1
-
-        return gradual_spd
 
     @staticmethod
     def clear_text(text) -> str:
