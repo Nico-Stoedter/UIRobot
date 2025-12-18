5ef30e95318dbaf243b3c0098f89d55ea691f970
Nico Stoedter <nicht vorhanden>
2025-12-18 11:15:51 +0100

    Komentare

--- omnivac/pop_up.py
+++ omnivac/pop_up.py
@@ -8,17 +8,18 @@ class PopUp:
         self.msg_box = None  
 
     def show_popup(self, message: str):
+        # Only display pop-up if no other exist
         if self.msg_box is not None and self.msg_box.isVisible():
             return 
 
         self.msg_box = QMessageBox()
         self.msg_box.setWindowTitle("Info Box")
         
-        # Eigenes Layout für Text + Spacer
+        # Own layout for pop-up
         text_widget = QWidget()
         v_layout = QVBoxLayout(text_widget)
-        v_layout.setContentsMargins(50, 20, 50, 20)  # gleichmäßige Ränder
-        v_layout.setSpacing(10)  # Abstand zwischen Text und Spacer
+        v_layout.setContentsMargins(50, 20, 50, 20)  
+        v_layout.setSpacing(10)
 
         label = QLabel(message)
         label.setAlignment(Qt.AlignmentFlag.AlignCenter)
